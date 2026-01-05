564824d8d40e0b18f59639bc6b7799024a76c358
lyheng <lyhenghong251221@gmail.com>
2026-01-05 12:25:02 +0700

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/stu_api/StuApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuApiApplication.class, args);
+	}
+
+}
